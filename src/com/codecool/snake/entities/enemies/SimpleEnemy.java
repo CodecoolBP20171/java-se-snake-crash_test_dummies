f1fe52b121ed9e732fb4ca08f5063ff9184c04a4
martonMeszaros <marton.m95@gmail.com>
2017-09-05 11:51:06 +0200

    Changed SimpleEnemy behaviour ad window edges

--- src/com/codecool/snake/entities/enemies/SimpleEnemy.java
+++ src/com/codecool/snake/entities/enemies/SimpleEnemy.java
@@ -16,26 +16,29 @@ public class SimpleEnemy extends GameEntity implements Animatable, Interactable 
 
     private Point2D heading;
     private static final int damage = 10;
+    private static final int DEFAULT_SPEED = 2;
 
     public SimpleEnemy(Pane pane) {
         super(pane);
 
         setImage(Globals.simpleEnemy);
         pane.getChildren().add(this);
-        int speed = 1;
         Random rnd = new Random();
         setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
         setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
 
         double direction = rnd.nextDouble() * 360;
         setRotate(direction);
-        heading = Utils.directionToVector(direction, speed);
+        heading = Utils.directionToVector(direction, DEFAULT_SPEED);
     }
 
     @Override
     public void step() {
-        if (isOutOfBounds()) {
-            destroy();
+        // TODO reverse the direction of the enemy when bouncing form a wall
+        if (getX() > Globals.WINDOW_WIDTH || getX() < 0) {
+            heading = new Point2D(- heading.getX(), heading.getY());
+        } else if (getY() > Globals.WINDOW_HEIGHT || getY() < 0) {
+            heading = new Point2D(heading.getX(), - heading.getY());
         }
         setX(getX() + heading.getX());
         setY(getY() + heading.getY());
