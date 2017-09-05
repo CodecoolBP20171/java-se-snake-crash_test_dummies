55a7f8ef03e842b38ad7fb0a4ff2f8b0392863bc
AndrasKovacs84 <ka841001@gmail.com>
2017-09-05 14:28:00 +0200

    Two player mode with custom graphics

--- src/com/codecool/snake/Game.java
+++ src/com/codecool/snake/Game.java
@@ -9,7 +9,12 @@ import javafx.scene.layout.Pane;
 public class Game extends Pane {
 
     public Game() {
-        new SnakeHead(this, 500, 500);
+        if(Globals.coop) {
+            new SnakeHead(this, "Player1", 700, 500);
+            new SnakeHead(this, "Player2", 300, 500);
+        } else {
+            new SnakeHead(this, "Player1", 500, 500);
+        }
 
         new SimpleEnemy(this);
         new SimpleEnemy(this);
@@ -26,17 +31,23 @@ public class Game extends Pane {
         Scene scene = getScene();
         scene.setOnKeyPressed(event -> {
             switch (event.getCode()) {
-                case LEFT:  Globals.leftKeyDown  = true; break;
-                case RIGHT: Globals.rightKeyDown  = true; break;
+                case LEFT:  Globals.player1Left  = true; break;
+                case RIGHT: Globals.player1Right  = true; break;
+                case Q:  Globals.player2Left  = true; break;
+                case W: Globals.player2Right  = true; break;
             }
         });
 
         scene.setOnKeyReleased(event -> {
             switch (event.getCode()) {
-                case LEFT:  Globals.leftKeyDown  = false; break;
-                case RIGHT: Globals.rightKeyDown  = false; break;
+                case LEFT:  Globals.player1Left  = false; break;
+                case RIGHT: Globals.player1Right  = false; break;
+                case Q:  Globals.player2Left  = false; break;
+                case W: Globals.player2Right  = false; break;
+
             }
         });
+
         Globals.gameLoop = new GameLoop();
         Globals.gameLoop.start();
     }
